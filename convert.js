18c3548a16c99712e26e4afd400c567242b71524
powerfullz <wleizc@hotmail.com>
2025-10-12 04:58:07 +0200

    chore: 去广告规则换用性能更好的`.mrs`格式

--- convert.js
+++ convert.js
@@ -56,9 +56,9 @@ function buildBaseLists({ landing, lowCost, countryInfo }) {
 
 const ruleProviders = {
     "ADBlock": {
-        "type": "http", "behavior": "domain", "format": "text", "interval": 86400,
-        "url": "https://adrules.top/adrules_domainset.txt",
-        "path": "./ruleset/ADBlock.txt"
+        "type": "http", "behavior": "domain", "format": "mrs", "interval": 86400,
+        "url": "https://adrules.top/adrules-mihomo.mrs",
+        "path": "./ruleset/ADBlock.mrs"
     },
     "TruthSocial": {
         "url": "https://cdn.jsdelivr.net/gh/powerfullz/override-rules@master/ruleset/TruthSocial.list",
